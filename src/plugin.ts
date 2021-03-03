ed1c5523e600586dc759923d7647623596f86552
Jason Kuhrt <jasonkuhrt@me.com>
2021-03-02 22:13:10 -0500

    nexus plugin not available yet

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,7 +1,12 @@
 import {} from '@prisma/generator-helper'
+import endent from 'endent'
 import * as Nexus from 'nexus'
 import { d } from './helpers/debugNexusPrisma'
 import { settings, SettingsInput } from './settings'
+
+throw new Error(endent`
+  Nexus Prisma is currently only available as a Prisma geneator.
+`)
 
 export function plugin(settingsInput?: SettingsInput): Nexus.core.NexusPlugin {
   return Nexus.plugin({

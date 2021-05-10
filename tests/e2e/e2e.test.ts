9799316c0c2a94802502fa352618ad2504e895af
Jason Kuhrt <jasonkuhrt@me.com>
2021-05-10 17:58:41 -0400

    try bash envar set

--- tests/e2e/e2e.test.ts
+++ tests/e2e/e2e.test.ts
@@ -262,7 +262,10 @@ it('When bundled custom scalars are used the project type checks and generates e
     {
       filePath: `.env`,
       content: endent`
-        DB_URL="postgres://bcnfshogmxsukp:e31b6ddc8b9d85f8964b6671e4b578c58f0d13e15f637513207d44268eabc950@ec2-54-196-33-23.compute-1.amazonaws.com:5432/d17vadgam0dtao"
+        DB_URL="${
+          process.env.E2E_DB_URL ??
+          'postgres://bcnfshogmxsukp:e31b6ddc8b9d85f8964b6671e4b578c58f0d13e15f637513207d44268eabc950@ec2-54-196-33-23.compute-1.amazonaws.com:5432/d17vadgam0dtao'
+        }"
         NO_PEER_DEPENDENCY_CHECK="true"
       `,
     },

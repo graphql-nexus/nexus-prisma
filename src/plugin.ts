ddf40474d1f0614ba1cb41d3d34cc4a7219c0077
Iddan Aaronsohn <mail@aniddan.com>
2021-05-11 23:22:39 -0700

    Silence eslint

--- src/plugin.ts
+++ src/plugin.ts
@@ -6,7 +6,7 @@ import { d } from './helpers/debugNexusPrisma'
 import { SettingsInput, change } from './settings'
 
 throw new Error(endent`
-  Nexus Prisma is currently only available as a Prisma geneator.
+  Nexus Prisma is currently only available as a Prisma generator.
 `)
 
 export function plugin(settingsInput?: SettingsInput): Nexus.core.NexusPlugin {
@@ -14,7 +14,11 @@ export function plugin(settingsInput?: SettingsInput): Nexus.core.NexusPlugin {
     name: 'nexus-prisma',
     onInstall() {
       d('nexus plugin onInstall')
-      const settingsData = change({ ...settingsInput, prisma: new PrismaClient() }).data
+      const settingsData = change({
+        ...settingsInput,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+        prisma: new PrismaClient(),
+      }).data
       settingsData
     },
   })

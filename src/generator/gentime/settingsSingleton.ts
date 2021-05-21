db3b100f794e281c6dd6202cf7d5ec2c2bd726fa
Iddan Aaronsohn <mail@aniddan.com>
2021-05-21 18:24:39 +0300

    Add JSDoc documentation regarding docPropagation

--- src/generator/gentime/settingsSingleton.ts
+++ src/generator/gentime/settingsSingleton.ts
@@ -7,17 +7,26 @@ export namespace Gentime {
      */
     projectIdIntToGraphQL?: 'ID' | 'Int'
     /**
-     * TODO
+     * Should Prisma Schema docs propagate as docs?
+     *
+     * @default `true`
      */
     docPropagation?:
       | boolean
       | {
           /**
-           * TODO
+           * Should Prisma Schema docs propagate as JSDoc?
+           *
+           * @default `true`
            */
           JSDoc?: boolean
           /**
-           * TODO
+           * Should Prisma Schema docs propagate as GraphQL docs?
+           *
+           * @remarks When this is disabled it will force `.description` property to be `undefined`. This
+           *          is for convenience, allowing you to avoid post-generation data manipulation or
+           *          consumption contortions.
+           * @default `true`
            */
           GraphQLDocs?: boolean
         }

0da7a2c53c25e248c86feb8e93ae0811e8e355db
Jason Kuhrt <jasonkuhrt@me.com>
2021-08-24 15:47:20 -0400

    try windows fix

--- tests/e2e/ncc.test.ts
+++ tests/e2e/ncc.test.ts
@@ -8,6 +8,10 @@ it('works with ncc', async () => {
 
   await testProject.runOrThrowNpmScript(`build`)
 
+  // Remove this to ensure that when the ncc build is run in the next step
+  // it is truly running independent of any node_modules.
+  await testProject.fs.remove('node_modules')
+
   const result = await testProject.runOrThrowNpmScript(`start:dist`)
 
   expect(result.stdout).toMatchSnapshot()

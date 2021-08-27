06707ea04c58469079e6c10ad3492686f39e9b99
Jason Kuhrt <jasonkuhrt@me.com>
2021-08-27 15:55:16 -0400

    fix tests

--- jest.config.ts
+++ jest.config.ts
@@ -4,18 +4,6 @@ const config: InitialOptionsTsJest = {
   preset: 'ts-jest',
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
   testPathIgnorePatterns: process.env.CI ? [] : ['.*e2e.*'],
-  globals: {
-    'ts-jest': {
-      diagnostics: Boolean(process.env.CI)
-        ? {
-            // For some reason we get these diagnostic errors in CI but only sometimes
-            // locally and never in editor. Furthermore the errors are invalid since they are
-            // explicit any not implicit any! Example error in CI https://github.com/prisma/nexus-prisma/runs/2613820675#step:9:408
-            ignoreCodes: [7006, 7031],
-          }
-        : false,
-    },
-  },
   collectCoverageFrom: ['src/**/*'],
   coverageReporters: ['lcov', 'text', 'html'],
 }

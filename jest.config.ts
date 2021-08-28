a40dd10ead1eaef9b99e142bd65db1bbeb5a3368
Jason Kuhrt <jasonkuhrt@me.com>
2021-08-27 21:57:51 -0400

    test setup

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,14 @@ import type { InitialOptionsTsJest } from 'ts-jest/dist/types'
 
 const config: InitialOptionsTsJest = {
   preset: 'ts-jest',
-  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
-  testPathIgnorePatterns: process.env.CI ? [] : ['.*e2e.*'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+    'jest-watch-select-projects',
+  ],
   collectCoverageFrom: ['src/**/*'],
   coverageReporters: ['lcov', 'text', 'html'],
+  projects: [`<rootDir>/jest.config.e2e.ts`, `<rootDir>/jest.config.unit.ts`],
 }
 
 export default config
---
+++ jest.config.unit.ts
@@ -0,0 +1,9 @@
+import type { InitialOptionsTsJest } from 'ts-jest/dist/types'
+
+const config: InitialOptionsTsJest = {
+  preset: 'ts-jest',
+  displayName: 'unit',
+  testMatch: ['**/unit/**/*.test.ts', '**/lib/**/*.test.ts', '**/integration/**/*.test.ts'],
+}
+
+export default config

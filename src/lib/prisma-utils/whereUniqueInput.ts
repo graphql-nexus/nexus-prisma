4d4789c99202ff2366afd75f8b320549ca881e3e
James Pulec <jpulec@gmail.com>
2022-05-06 14:43:21 -0700

    Check for Primary Key Name for UniqueWhereInput

--- src/lib/prisma-utils/whereUniqueInput.ts
+++ src/lib/prisma-utils/whereUniqueInput.ts
@@ -54,6 +54,9 @@ function getUniqueIdentifierFields(model: DMMF.Model): FieldName[] {
 
   // Try finding 2
   if (model.primaryKey && model.primaryKey.fields.length > 0) {
+    if (model.primaryKey.name) {
+      return [model.primaryKey.name];
+    }
     return model.primaryKey.fields
   }
 

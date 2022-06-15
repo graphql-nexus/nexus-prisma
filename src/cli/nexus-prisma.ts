39ec45bf806a71cecedc558035eb83d1c55d0a17
Ahmed Elywa <ahmed.elywa@icloud.com>
2022-06-15 11:39:09 +0200

    Use the default @prisma/client package if the user using pnpm package (#220)
    * use the default prisma client package if user using pnpm package
    
    * fix the test
    
    * add todo for the test

--- src/cli/nexus-prisma.ts
+++ src/cli/nexus-prisma.ts
@@ -135,6 +135,16 @@ function getPrismaClientImportIdForItsGeneratorOutputConfig(
     return prismaClientPackageMoniker
   }
 
+  /**
+   * when using a pnpm package manager it's generate a custom folder for every package and breaking our import.
+   * as a fix if we found that the prisma output path include the .pnpm folder we use the prismaClientPackageMoniker
+   * TODO: adding a test https://github.com/prisma/nexus-prisma/issues/189
+   */
+  const prismaClientOutputPNPM = Path.normalize(`/node_modules/.pnpm/@prisma+client`)
+  if (prismaClientGeneratorConfig.output.value.includes(prismaClientOutputPNPM)) {
+    return prismaClientPackageMoniker
+  }
+
   if (prismaClientGeneratorConfig.output.value.endsWith(prismaClientDefaultOutput)) {
     /**
      * Goal of this code:

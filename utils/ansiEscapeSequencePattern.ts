3505319b9e2bf49ef6efe4be48d39438598f4e69
Ahmed Elywa <ahmed.elywa@icloud.com>
2022-06-30 18:20:24 +0200

    eslint-disable-next-line no-control-regex

--- utils/ansiEscapeSequencePattern.ts
+++ utils/ansiEscapeSequencePattern.ts
@@ -1,4 +1,5 @@
 /**
  * @see https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
  */
+// eslint-disable-next-line no-control-regex
 export const ansiEscapeSequencePattern = /\x1b\[[0-9]+m/g

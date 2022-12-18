cc3a3d84c2e1dc1e86680d35a99276e6e132ed7e
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2022-12-18 14:27:51 +0100

    ci: unify test commit status check title

--- tests/e2e/kitchen-sink.test.ts
+++ tests/e2e/kitchen-sink.test.ts
@@ -330,7 +330,7 @@ it('A full-featured project type checks, generates expected GraphQL schema, and 
     /.*"prismaClientImportId": "@prisma\/client".*/
   )
 
-  if (process.env.DATABASE_SETUP === 'not-available') {
+  if (process.env.DATABASE === 'no-db') {
     d(`database not available, skipping runtime test`)
     return
   }

72a07ce8ed9fc5cb7f75c0c7f72d88e5ad4ad92b
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2022-12-27 20:09:10 +0000

    Fixed spellings (#281)
    * Fixed spellings
    
    * Updated snapshot
    
    Co-authored-by: Rostislav Simonik <rostislav.simonik@technologystudio.sk>

--- src/generator/helpers/JSDocTemplates.ts
+++ src/generator/helpers/JSDocTemplates.ts
@@ -217,7 +217,7 @@ const missingDocsIntro = (
       : info.kind
 
   return dedent`
-     * ### ️⚠️ You have not writen documentation for ${thisItem}
+     * ### ️⚠️ You have not written documentation for ${thisItem}
      *
      * Replace this default advisory JSDoc with your own documentation about ${thisItem}
      * by documenting it in your Prisma schema. For example:

4695f4e6b2d2cfe7ed7d28117ad8dba05f31c88a
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2023-05-15 22:23:43 +0200

    style: fix formatting

--- tests/e2e/run-or-throw.ts
+++ tests/e2e/run-or-throw.ts
@@ -6,7 +6,7 @@ export const bindRunOrThrow = (ctx: any) => {
       const start = Date.now()
       const result = internal(...args)
       const end = Date.now()
-      const diff = (end - start)/1000
+      const diff = (end - start) / 1000
       console.log(`END (${diff}s)`, args[0])
       return result
     }

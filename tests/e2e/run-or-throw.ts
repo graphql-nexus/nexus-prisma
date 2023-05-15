fccf06a2e45f2a585e5d41d3e3ebe67ec32f1654
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2023-05-15 22:23:01 +0200

    debug: add duration measurement in runOrThrow wrapping

--- tests/e2e/run-or-throw.ts
+++ tests/e2e/run-or-throw.ts
@@ -3,8 +3,11 @@ export const bindRunOrThrow = (ctx: any) => {
     const internal = ctx[key]
     ctx[key] = (...args: any[]) => {
       console.log('START:', args[0])
+      const start = Date.now()
       const result = internal(...args)
-      console.log('END', args[0])
+      const end = Date.now()
+      const diff = (end - start)/1000
+      console.log(`END (${diff}s)`, args[0])
       return result
     }
   }

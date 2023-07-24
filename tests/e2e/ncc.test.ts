444363850b25fcd199939b7113e151697911fa12
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2023-07-24 21:17:12 +0200

    debug: remove debug console.log outputs

--- tests/e2e/ncc.test.ts
+++ tests/e2e/ncc.test.ts
@@ -9,15 +9,11 @@ const ctx = konn().useBeforeEach(providers.dir()).useBeforeEach(run()).useBefore
 it(
   'works with ncc',
   async () => {
-    console.log('ncc.1')
     expect.assertions(1)
-    console.log('ncc.2')
     await ctx.fixture.useAsync(Path.join(__dirname, 'fixtures/ncc'))
-    console.log('ncc.3')
     await ctx.runAsyncOrThrow(
       `${Path.join(process.cwd(), 'node_modules/.bin/yalc')} add ${ctx.thisPackageName}`
     )
-    console.log('ncc.4')
     await monitorAsyncMethod(
       () =>
         ctx.runPackagerCommandAsyncOrThrow('install --legacy-peer-deps --prefer-offline', {
@@ -25,17 +21,13 @@ it(
         }),
       { retry: 3, timeout: 90 * 1000 }
     )
-    console.log('ncc.5')
     await ctx.runPackagerCommandAsyncOrThrow('build')
-    console.log('ncc.6')
     // Remove this to ensure that when the ncc build is run in the next step
     // it is truly running independent of any node_modules.
     await ctx.fs.removeAsync('node_modules')
-    console.log('ncc.7')
     const result = await ctx.runPackagerCommandAsyncOrThrow('run --silent start:dist', {
       env: { PEER_DEPENDENCY_CHECK: 'false' },
     })
-    console.log('ncc.8')
     expect(stripEndingLines(result.stdout)).toMatchSnapshot()
   },
   320 * 1000

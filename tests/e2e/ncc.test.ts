859d18014a89fbcc7b76fb320519c4169722d6e3
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2023-07-24 20:47:52 +0200

    test: prefer offline installation of dependencies in tests

--- tests/e2e/ncc.test.ts
+++ tests/e2e/ncc.test.ts
@@ -14,7 +14,7 @@ it('works with ncc', async () => {
   )
   await monitorAsyncMethod(
     () =>
-      ctx.runPackagerCommandAsyncOrThrow('install --legacy-peer-deps', {
+      ctx.runPackagerCommandAsyncOrThrow('install --legacy-peer-deps --prefer-offline', {
         env: { PEER_DEPENDENCY_CHECK: 'false' },
       }),
     { retry: 3, timeout: 90 * 1000 }

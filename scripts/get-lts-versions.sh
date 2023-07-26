1d65a9a0955ec8859aadb5e80d116ba3772417ab
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2023-07-26 08:38:27 +0200

    ci: add dynamic retrieval lts nodejs version in ci workflows (#680)
    * ci: add dynamic retrieval lts nodejs version in ci workflows
    
    * fix: return single number instead of array with one item in get-lts-versions for last_lts_version
    
    * fix: set propare script file name for retrieval nodejs lts matrix
    
    * fix: refactor get-e2e-db-schema.ts to support dynamic nodejs version retrieval
    
    * style: fix formatting

---
+++ scripts/get-lts-versions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -exo pipefail
+
+json=$(curl -s https://raw.githubusercontent.com/nodejs/Release/master/schedule.json)
+
+current_date=$(date +%Y-%m-%d)
+
+lts_versions=$(echo "$json" | jq -c "[ to_entries[] | select(.value.lts != null and .value.maintenance >= \"$current_date\" and .value.end >= \"$current_date\") | .key[1:] | tonumber ] | sort ")
+last_lts_version=$(echo "$json" | jq -c "[ to_entries[] | select(.value.lts != null and .value.maintenance >= \"$current_date\" and .value.end >= \"$current_date\") | .key[1:] | tonumber ] | sort | .[-1]")
+echo "matrix_lts_versions=$lts_versions" >> $GITHUB_OUTPUT
+echo "matrix_last_lts_version=[$last_lts_version]" >> $GITHUB_OUTPUT
+echo "last_lts_version=$last_lts_version" >> $GITHUB_OUTPUT

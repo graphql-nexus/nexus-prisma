c05b178cab5e5930159c388ad0b3a19941b24792
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2023-07-28 09:15:46 +0200

    chore: introduce lint-staged (#684)
    * chore: introduce lint-staged
    * fix: remove redundant lint-staged config in package.json

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  '*': 'prettier --ignore-unknown --check',
+  '*.{ts,tsx}': (filenames) => [
+    `yarn eslint --ext .ts,.tsx --max-warnings 0 ${filenames.join(' ')}`,
+    'yarn type:check',
+  ],
+}

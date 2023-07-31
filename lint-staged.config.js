cf3f44bf3ad11aaf4c45e84dacef2a5cc365b2cc
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2023-07-31 18:36:04 +0200

    chore: enforce tsc skipLibCheck on lint-staged due broken dependencies

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,6 @@ module.exports = {
   '*': 'prettier --ignore-unknown --check',
   '*.{ts,tsx}': (filenames) => [
     `yarn eslint --ext .ts,.tsx --max-warnings 0 ${filenames.join(' ')}`,
-    'yarn type:check',
+    'yarn type:check --skipLibCheck',
   ],
 }
